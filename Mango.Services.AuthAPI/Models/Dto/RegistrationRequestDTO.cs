6efb10a4ccfb32deae76ea4a4da3f8988ae8fc35
Jenil Vora <jenil.vora0408@gmail.com>
2023-11-28 09:43:32 +0530

    Completed Coupon API, integrated in frontend

---
+++ Mango.Services.AuthAPI/Models/Dto/RegistrationRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class RegistrationRequestDTO
+    {
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+    }
+}
